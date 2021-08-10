694320567926071d5e082004fe396696fb2bd24c
Pikuman83 <wahab_anjum@hotmail.com>
2021-08-10 19:27:35 +0200

    'Initial-setup'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'projects', loadChildren: () => import('./modules/projects/projects.module').then(m => m.ProjectsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
