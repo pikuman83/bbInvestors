d212feb7a3eb1f19945719c0ae430ff845fefdc3
Pikuman83 <wahab_anjum@hotmail.com>
2021-08-11 16:08:02 +0200

    'localization'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'bbInvestors';
+  siteLanguage: string = 'ES';
+  siteLocale: string = '';
+  languageList = ['ES','EN','FR'];
+  
+  ngOnInit() {
+    setTimeout(() => {
+      this.siteLocale = window.location.pathname.split('/')[1];  
+    }, );
+    
+    // this.siteLanguage = this.languageList.find(f => f.code === this.siteLocale).label;
+    console.log(this.siteLocale.toUpperCase());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,13 @@ import { AppComponent } from './app.component';
 
 import { StaticContentModule } from "./modules/static-content/static-content.module";
 import { ProjectsModule } from "./modules/projects/projects.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+// import { MatSelectModule } from '@angular/material/select';
+// import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -15,7 +22,11 @@ import { ProjectsModule } from "./modules/projects/projects.module";
     BrowserModule,
     AppRoutingModule,
     StaticContentModule,
-    ProjectsModule
+    ProjectsModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
