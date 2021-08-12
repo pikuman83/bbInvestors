ade1074fe393c3a4b9bbefb18aeb23b34c85da32
Pikuman83 <wahab_anjum@hotmail.com>
2021-08-13 01:58:22 +0200

    'about-page'

--- src/app/modules/static-content/static-content.module.ts
+++ src/app/modules/static-content/static-content.module.ts
@@ -5,6 +5,7 @@ import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
 import { NewsComponent } from './news/news.component';
+import { AppRoutingModule } from '../../app-routing.module';
 
 
 
@@ -17,7 +18,8 @@ import { NewsComponent } from './news/news.component';
     NewsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ]
 })
 export class StaticContentModule { }
