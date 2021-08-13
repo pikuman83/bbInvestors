5bf7efdae76ca27b158376ea4fede689a39991fc
Pikuman83 <wahab_anjum@hotmail.com>
2021-08-13 19:42:24 +0200

    'services-partial'

--- src/app/modules/static-content/services/services.component.ts
+++ src/app/modules/static-content/services/services.component.ts
@@ -11,5 +11,6 @@ export class ServicesComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  card: string = "Le acompañaré y guiaré en todo el proceso de invertir en mercados internacionales de manera controlada y segura. Trabajaremos juntos para conseguir los objetivos marcados:"
 
 }
