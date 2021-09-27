58e449d1318c943bed75d4f98b5734c05baf2078
Pikuman83 <wahab_anjum@hotmail.com>
2021-09-27 12:27:22 +0200

    'production-bug-fixes'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,12 +20,13 @@ export class AppComponent implements OnInit, OnDestroy{
   constructor(public service: GlobalService, @Inject(DOCUMENT) private _document: Document, private _renderer2: Renderer2,
     public dialog: MatDialog,
     public auth: AuthService
-    ){service.showMenu.subscribe((x) => {
+    ){const route = service.showMenu.subscribe((x) => {
         this.showMenu = x;
-        // if(x){
-        //   console.log('Came from landing')
-        //   this.writeScript();
-        // }
+        if(x){
+          route.unsubscribe();
+          // console.log('Came from landing')
+          // this.writeScript();
+        }
       })
     }
 
@@ -54,6 +55,12 @@ export class AppComponent implements OnInit, OnDestroy{
       var myZadarmaCallmeWidgetFn10387 = function() {
         myZadarmaCallmeWidget10387 = new ZadarmaCallmeWidget("myZadarmaCallmeWidget10387");
         myZadarmaCallmeWidget10387.create({"widgetId": "rNnfyspG7tuAf4824urn9vasFU8pzh7DdDe5ZfvgMcL2kCxH8xL5rAbP9Z5u2pGDmb8Rru8hz4Mhjs3u54aSNeykFm12ufZ2cb14c74454d4170824956ddfa828ffbf", "sipId":"267956", "domElement":"myZadarmaCallmeWidget10387" }, { "shape":"square", "language":"es", "width":"0", "dtmf":false, "font": "'Trebuchet MS','Helvetica CY',sans-serif", "color_call": "rgb(255, 255, 255)", "color_bg_call": "rgb(126, 211, 33)", "color_border_call": "rgb(191, 233, 144)", "color_connection": "rgb(255, 255, 255)", "color_bg_connection": "rgb(33, 211, 166)", "color_border_connection": "rgb(144, 233, 211)", "color_calling": "rgb(255, 255, 255)", "color_border_calling": "rgb(255, 218, 128)", "color_bg_calling": "rgb(255, 181, 0)", "color_ended": "rgb(255, 255, 255)", "color_bg_ended": "rgb(164,164,164)", "color_border_ended": "rgb(210, 210, 210)"});}
+
+        if (window.addEventListener) {
+          window.addEventListener('load', myZadarmaCallmeWidgetFn10387, false);
+        } else if (window.attachEvent) {
+          window.attachEvent('onload', myZadarmaCallmeWidgetFn10387);
+        }
     }`;
     this._renderer2.appendChild(this._document.body, script);
     console.log('writing script', script)
