74f71f90f6ed3e6c7220943f59bd0d04d9a26bf9
Pikuman83 <wahab_anjum@hotmail.com>
2021-10-06 01:20:48 +0200

    'contact-form-submission'

--- src/app/modules/static-content/contact/contact.component.ts
+++ src/app/modules/static-content/contact/contact.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
+import { FireStoreService } from 'src/app/core/fire-store.service';
 
 @Component({
   selector: 'app-contact',
@@ -16,7 +17,8 @@ export class ContactComponent implements OnInit {
     movil: ['', Validators.minLength(9)],
     email: ['', [Validators.required, Validators.email]],
     message: ['', [Validators.required, Validators.minLength(5)]],
-    terms: [false, [Validators.requiredTrue]]
+    terms: [false, [Validators.requiredTrue]],
+    time: [new Date()]
   });
 
   showHeader = false;
@@ -24,7 +26,8 @@ export class ContactComponent implements OnInit {
   constructor(
     private fb: FormBuilder, 
     private _snackBar: MatSnackBar,
-    private route: ActivatedRoute) {}
+    private route: ActivatedRoute,
+    private service: FireStoreService) {}
 
   ngOnInit(): void {
     this.showHeader = !!this.route.snapshot.paramMap.get('param');
@@ -32,25 +35,29 @@ export class ContactComponent implements OnInit {
 
   onSubmit(form:FormGroup) {
     if (!form.value.terms){
-      this._snackBar.open(`${$localize}:@@snackbar Must accept terms and conditions`,'BBInvestors');
+      this._snackBar.open(`Must accept terms and conditions`,'BBInvestors');
       return
     }
     else{
       if (this.contactForm.valid){
         const msg = {
           to: form.value.email,
-          from: 'wahab_anjum@hotmail.com',
-          subject: form.value.asunto,
-          text: form.value.message,
-          html: `<strong>New email from the web ${form.value.name}</strong>`,
-        };
-        async () => {
-          try {
-            console.table(msg)
-          } catch (error) {
-            console.error(error);
+          from: 'Briggite<wahab_anjum@hotmail.com>',
+          bcc: 'wahab_anjum@hotmail.com',
+          message: {
+            subject: `${form.value.asunto} con BB for Investors`,
+            html: `<strong>Hello ${form.value.name}!!</strong> Thank you very much for contacting, 
+            we have received your message and will get back to you as soon as possible. <br>
+            Message details: ${form.value.message}`,
           }
         };
+        try {
+          this.service.create('contacts', msg).then(()=>{
+            this._snackBar.open(`Thank you for contacting us, we will come back to you as soon as possible`,'BBInvestors', {panelClass: 'happy'});
+          })
+        } catch (error: any) {
+          this._snackBar.open(error.message,'BBInvestors');
+        }
       }
     }
   }
